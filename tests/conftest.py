051cec50820b4cac5a8f751e450d06c7f5635b91
Jon Banafato <jon@jonafato.com>
2015-12-28 11:48:06 -0500

    Use producer.app.name instead of producer.app_name
    Producer classes provided by Henson-SQS and Henson-AMQP have no
    attribute called ``app_name``. Instead, a reference to the Application
    object itself was added to both of these. This change allows calls to
    ``send_error`` and ``send_message`` to work with the current (master)
    versions of the plugins being used.

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,8 +16,36 @@ class Application:
         self.settings = settings
 
 
+class Producer:
+    """A stub producer that can be used for testing.
+
+    Args:
+        app: The application for which this producer produces.
+    """
+
+    def __init__(self, app):
+        """Initialize the instance."""
+        self.app = app
+        self.sent_error = None
+        self.sent_message = None
+
+    async def error(self, message):
+        """Mock send an error message."""
+        self.sent_error = message
+
+    async def send(self, message):
+        """Mock send a message."""
+        self.sent_message = message
+
+
 @pytest.fixture
 def test_app():
     """Return a test application."""
     app = Application()
     return app
+
+
+@pytest.fixture
+def test_producer(test_app):
+    """Return a test producer."""
+    return Producer(test_app)
