7bb854913bdec67b6ccf37058c2ea3905dcc1ff7
Zack Morris <zackmorris@iheartmedia.com>
2018-09-13 11:10:37 -0400

    Increment version to 2.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pipeline',
-    version='2.1.1',
+    version='2.2.1',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Henson>=0.5.0',
