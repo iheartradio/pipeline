3806f0e46d5b097933119dacde2819d6c400788e
Christian Paul <christianpaul@iheartmedia.com>
2021-03-30 18:56:17 -0400

    rename this branch of the library

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pipeline',
-    version='2.4.0',
+    name='pipeline-scaling-ihr',
+    version='0.1.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Henson>=0.5.0',
